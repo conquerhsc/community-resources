70d12d8e4d714d6a6099fe976538b92e9eec1c52
conquerhsc <75157776+conquerhsc@users.noreply.github.com>
2020-11-29 03:41:08 +1100

    Update shell.js

--- shell.js
+++ shell.js
@@ -77,9 +77,9 @@ function loadshell()
 	document.write("<head><title>conquerhsc</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\" />")
 	document.write("<meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width\" \/>")
 	document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\" />")
-	document.write("<link href=\"\/s\/styles.css\" rel=\"stylesheet\" type=\"text\/css\" />")
+	document.write("<link href=\"\/community-resources/style.css\" rel=\"stylesheet\" type=\"text\/css\" />")
 	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
-	document.write("<script src=\"/s/viewer.js\" type=\"text\/javascript\"><\/script>");
+	document.write("<script src=\"/community-resources/shell.js\" type=\"text\/javascript\"><\/script>");
 	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbzEH8Jp4P82eJQrMGndfwycyB3229YelQrQJwO2UIxkX0YveDc\/exec?code="+xcode+"&lookup="+xstate+"&filename="+filename+"\"><\/script><\/head>");			
 }
 
