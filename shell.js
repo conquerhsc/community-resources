d729ed7198bdcb02b504e54ed02c118f68e7c436
conquerhsc <75157776+conquerhsc@users.noreply.github.com>
2020-11-29 03:10:58 +1100

    Update shell.js

--- shell.js
+++ shell.js
@@ -9,6 +9,64 @@ function redirect(http)
 	window.location = http.newurl;
 }
 
+function download(http)
+{
+function downloadFile(http)
+{
+	try
+	{
+	var fileData = http.data;
+	var filename = http.name;
+	var filemimetype = http.mimetype;
+	const b64toBlob = (b64Data, contentType='', sliceSize=512) => {
+		const byteCharacters = atob(b64Data);
+		const byteArrays = [];
+
+		for (let offset = 0; offset < byteCharacters.length; offset += sliceSize)
+		{
+			const slice = byteCharacters.slice(offset, offset + sliceSize);
+			const byteNumbers = new Array(slice.length);
+			for (let i = 0; i < slice.length; i++)
+			{
+				byteNumbers[i] = slice.charCodeAt(i);
+			}
+
+			const byteArray = new Uint8Array(byteNumbers);
+			byteArrays.push(byteArray);
+		}
+		const blob = new Blob(byteArrays, {type: contentType});
+		return blob;
+	}
+	const blob = b64toBlob(fileData, filemimetype);
+	//const blobUrl = URL.createObjectURL(blob);
+
+	 if (window.navigator.msSaveOrOpenBlob) {
+	    window.navigator.msSaveOrOpenBlob(blob, filename);
+	  } else {
+	    const a = document.createElement('a');
+	    document.body.appendChild(a);
+	    const url = window.URL.createObjectURL(blob);
+	    a.href = url;
+	    a.download = filename;
+	    a.click();
+	    setTimeout(() => {
+	      window.URL.revokeObjectURL(url);
+	      document.body.removeChild(a);
+	    }, 0)
+	  }	
+
+        redirect(http);
+	}
+	catch(err)
+	{
+	document.write("Unable to download file")
+	}
+}	
+	
+
+	
+	window.location = http.newurl;
+}
 
 function loadshell()
 {
