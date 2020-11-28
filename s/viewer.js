200a310c0ad51b60919aec5f688a4c1f366aed7a
conquerhsc <75157776+conquerhsc@users.noreply.github.com>
2020-11-28 18:57:45 +1100

    Update viewer.js

--- s/viewer.js
+++ s/viewer.js
@@ -7,8 +7,7 @@ function writeshell(http)
 
 function loadshell()
 {
-	var url = window.location.pathname;
-	var lookupvalue = url.split("/community-resources/")[1]
+	var url = window.location.pathname.substring(1);
 	
 	document.write("<head><title>conquerhsc</title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\" />")
 	document.write("<meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width\" \/>")
@@ -16,7 +15,7 @@ function loadshell()
 	document.write("<link href=\"\/s\/styles.css\" rel=\"stylesheet\" type=\"text\/css\" />")
 	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
 	document.write("<script src=\"/s/viewer.js\" type=\"text\/javascript\"><\/script>");
-	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbzEH8Jp4P82eJQrMGndfwycyB3229YelQrQJwO2UIxkX0YveDc\/exec?lookup="+ lookupvalue +"\"><\/script><\/head>");			
+	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbzEH8Jp4P82eJQrMGndfwycyB3229YelQrQJwO2UIxkX0YveDc\/exec?lookup="+ url +"\"><\/script><\/head>");			
 }
 
 function passwordentry()
