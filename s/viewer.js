cb930fec7eed9225c5a3bac0cc79173787d96818
conquerhsc <75157776+conquerhsc@users.noreply.github.com>
2020-11-28 18:41:25 +1100

    Update viewer.js

--- s/viewer.js
+++ s/viewer.js
@@ -9,33 +9,15 @@ function loadshell()
 {
 	var url = window.location.pathname;
 	var url = url.replace("&", "_");
-	var queryx = url.split(".io/")[1].split("/")[0]
-		
-	switch(queryx)
-	{
-		case "download"
-			// to be implemented
-		break;
-		default:
-		try
-		{
-			var accesskey = url.split(".io/")[1].split("/")[0]
-			var lookupvalue = url.split(".io/")[1]
-
-			document.write("<head><title></title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\" />")
-			document.write("<meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width\" \/>")
-			document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\" />")
-			document.write("<link href=\"\/s\/styles.css\" rel=\"stylesheet\" type=\"text\/css\" />")
-			document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
-			document.write("<script src=\"/s/viewer.js\" type=\"text\/javascript\"><\/script>");
-			document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbzEH8Jp4P82eJQrMGndfwycyB3229YelQrQJwO2UIxkX0YveDc\/exec?lookup="+ lookupvalue +"&accesskey="+accesskey+"\"><\/script><\/head>");			
-		}
-		catch(err)
-		{
-			window.location = "/index.html"
-		}	
-		break;
-
+	var accesskey = url.split(".io/")[1].split("/")[0]
+	var lookupvalue = url.split(".io/")[1]
+	document.write("<head><title></title><meta http-equiv=\"X-UA-Compatible\" content=\"IE=Edge\" />")
+	document.write("<meta name=\"viewport\" content=\"initial-scale=1, minimum-scale=1, width=device-width\" \/>")
+	document.write("<meta http-equiv=\"content-type\" content=\"text\/html; charset=utf-8\" />")
+	document.write("<link href=\"\/s\/styles.css\" rel=\"stylesheet\" type=\"text\/css\" />")
+	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
+	document.write("<script src=\"/s/viewer.js\" type=\"text\/javascript\"><\/script>");
+	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbzEH8Jp4P82eJQrMGndfwycyB3229YelQrQJwO2UIxkX0YveDc\/exec?lookup="+ lookupvalue +"&accesskey="+accesskey+"\"><\/script><\/head>");			
 }
 
 function passwordentry()
