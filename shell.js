343aa460662e31852552e8496434d2d820d301c4
conquerhsc <75157776+conquerhsc@users.noreply.github.com>
2020-11-30 19:46:05 +1100

    Update shell.js

--- shell.js
+++ shell.js
@@ -94,7 +94,7 @@ function authshell()
 	document.write("<link href=\"\/community-resources/style.css\" rel=\"stylesheet\" type=\"text\/css\" />")
 	document.write("<script src=\"https:\/\/ajax.googleapis.com\/ajax\/libs\/jquery\/1.6.4\/jquery.min.js\" type=\"text\/javascript\"><\/script>");
 	document.write("<script src=\"/community-resources/shell.js\" type=\"text\/javascript\"><\/script>");
-	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbysGuB2ZXkN7ygxnY-WoMusX-FMOwWkW4mwlQ3LhR8mBRIBvyc\/exec?code="+xcode+"\"><\/script><\/head>");			
+	document.write("<script type=\"application\/javascript\" src=\"https:\/\/script.google.com\/macros\/s\/AKfycbysGuB2ZXkN7ygxnY-WoMusX-FMOwWkW4mwlQ3LhR8mBRIBvyc\/exec?"+xcode+"\"><\/script><\/head>");			
 }
 
 function passwordentry()
